050daf4f856a12af9faebd8205368336206233d6
hanumantharao <hanumantharao1986.medikonda@gmail.com>
2025-09-10 19:16:32 +0530

    create gke cluster

--- terraform/18_gke-public-module/main.tf
+++ terraform/18_gke-public-module/main.tf
@@ -1,1 +1,106 @@
-# this 9th class
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = "pure-silicon-463515-t5"
+  name                       = "gke-test-1"
+  region                     = "us-west1"
+  zones                      = ["us-west1-a", "us-west1-b", "us-west1-c"]
+  network                    = "example-vpc"
+  subnetwork                 = "subnet-01"
+  ip_range_pods              = "pod-ip-range"
+  ip_range_services          = "service-ip-range"
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+  dns_cache                  = false
+  remove_default_node_pool   = true
+  deletion_protection = false
+
+  node_pools = [
+    {
+      name                        = "default-node-pool"
+      machine_type                = "e2-medium"
+      node_locations              = "us-west1-a,us-west1-b"
+      min_count                   = 1
+      max_count                   = 3
+      local_ssd_count             = 0
+      spot                        = false
+      disk_size_gb                = 100
+      disk_type                   = "pd-standard"
+      image_type                  = "COS_CONTAINERD"
+      enable_gcfs                 = false
+      enable_gvnic                = false
+      logging_variant             = "DEFAULT"
+      auto_repair                 = true
+      auto_upgrade                = true
+      service_account             = "hanu-terraform@pure-silicon-463515-t5.iam.gserviceaccount.com"
+      preemptible                 = false
+      initial_node_count          = 1
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+  }
+}
+
+resource "google_project_service" "required" {
+  for_each = toset([
+    "container.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "compute.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "dns.googleapis.com",
+  ])
+
+  project = "pure-silicon-463515-t5"
+  service = each.key
+
+  disable_on_destroy = false
+}
